7159e391e5a2c6460998aedc466c831240b42772
woai3c <411020382@qq.com>
2024-08-17 23:36:00 +0800

    chore: replace owner check logic with 'onlyOwner' modifier

--- contracts/PaymentProcessor.sol
+++ contracts/PaymentProcessor.sol
@@ -2,15 +2,14 @@
 pragma solidity ^0.8.19;
 
 import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
 
-contract PaymentProcessor {
+contract PaymentProcessor is Ownable {
   IERC20 public token;
-  address public owner;
   uint256 public pricePerLiter = 1;
 
   error InsufficientBalance(uint256 available, uint256 required);
   error TransferFailed();
-  error OnlyOwner();
 
   event PaymentReceived(address indexed from, uint256 amount, uint256 liters);
   event Withdrawal(address indexed to, uint256 amount);
@@ -18,7 +17,6 @@ contract PaymentProcessor {
 
   constructor(address tokenAddress) {
     token = IERC20(tokenAddress);
-    owner = msg.sender;
   }
 
   function pay(uint256 amount) external {
@@ -35,28 +33,20 @@ contract PaymentProcessor {
     emit PaymentReceived(msg.sender, amount, liters);
   }
 
-  function withdraw(uint256 amount) external {
-    if (msg.sender != owner) {
-      revert OnlyOwner();
-    }
-
+  function withdraw(uint256 amount) external onlyOwner {
     if (token.balanceOf(address(this)) < amount) {
       revert InsufficientBalance(token.balanceOf(address(this)), amount);
     }
 
-    bool success = token.transfer(owner, amount);
+    bool success = token.transfer(owner(), amount);
     if (!success) {
       revert TransferFailed();
     }
 
-    emit Withdrawal(owner, amount);
+    emit Withdrawal(owner(), amount);
   }
 
-  function setPricePerLiter(uint256 newPrice) external {
-    if (msg.sender != owner) {
-      revert OnlyOwner();
-    }
-
+  function setPricePerLiter(uint256 newPrice) external onlyOwner {
     uint256 oldPrice = pricePerLiter;
     pricePerLiter = newPrice;
     emit PricePerLiterChanged(oldPrice, newPrice);
